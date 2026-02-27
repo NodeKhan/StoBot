d5e76a3c55b81d8fcfcaa88a89c618776d6fc442
NodeKhan_pc <richard.dejamblinne@proton.me>
2026-02-27 12:26:56 +0100

    update botinitilizer for multiple workflow (prod & dev) + fix small mistake in deploy.yml

--- src/main/java/bot/stock/stobot/bot/core/BotInitializer.java
+++ src/main/java/bot/stock/stobot/bot/core/BotInitializer.java
@@ -1,76 +1,147 @@
 package bot.stock.stobot.bot.core;
 
-import bot.stock.stobot.bot.features.kill.KillListener;
 import jakarta.annotation.PostConstruct;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.hooks.EventListener;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Component
-public class BotInitializer {
+public class BotInitializer{
 
-    private final JDA jda;
-    private final List<EventListener> listeners;
-    private final List<SlashCommandProvider> commandProviders;
-    private final String guildId;
-    private final KillListener killListener;
-
+    private final CommandContext ctx;
+    private final CommandRegister register;
+    
+    //base call
     public BotInitializer(
-            JDA jda,
-            List<EventListener> listeners,
-            List<SlashCommandProvider> commandProviders,
-            @Value("${discord.guild-id}") String guildId, KillListener killListener
-    ) {
-        this.jda = jda;
-        this.listeners = listeners;
-        this.commandProviders = commandProviders;
-        this.guildId = guildId;
-        this.killListener = killListener;
+        CommandContext ctx,
+        CommandRegister register
+    ){
+        this.ctx = ctx;
+        this.register = register;
     }
 
     @PostConstruct
-    public void init() {
-        //TODO : change for dev/product mode
-        for (EventListener l : listeners) {
-            jda.addEventListener(l);
-        }
+    public void init(){
+        register.register(ctx);
+    }
 
-        List<CommandData> commands = new ArrayList<>();
-        for (SlashCommandProvider cp : commandProviders) {
-            commands.add(cp.command());
-            System.out.println(cp.command());
-        }
+    //context to not repeat injection and code.
+    //is use to esquive repetitive code
+    @Component
+    protected static class CommandContext {
+        final JDA jda;
+        final List<EventListener> listeners;
+        final Guild guild;
+        final CommandRegister registrar;
 
-        Guild guild = jda.getGuildById(guildId);
-        if (guild != null) {
-            try {
-                guild.updateCommands()
-                        .addCommands(commands)
-                        .addCommands(killListener.command())
-                        .complete();
-                System.out.println("Registered " + commands.size() + " commands for guild " + guild.getName());
-                System.out.println("Kill switch added");
+        public CommandContext(
+            JDA jda,
+            List<EventListener> listeners,
+            @Value("${discord.guild-id}") String GuildID,
+            CommandRegister registrar
+        ){
+            this.jda = jda;
+            this.listeners = listeners;
+            this.registrar = registrar;
 
-            } catch (Exception e) {
-                System.err.println("Failed to register commands: " + e.getMessage());
+            if(GuildID == null){
+                System.err.println("discord.dev-guild-id is required");
             }
 
-        } else {
-            System.err.println("Guild not found: " + guildId);
+            this.guild = jda.getGuildById(GuildID);
         }
 
-//        jda.updateCommands()
-//                .addCommands(commands)
-//                .queue(
-//                        ok -> System.out.println("Registered " + commands.size() + " commands for world"),
-//                        err -> System.err.println("Failed to register commands: " + err.getMessage())
-//                );
+    }
+
+    //add commande
+    interface CommandRegister {
+    void register(CommandContext ctx);
+    }
+
+    //dev profil, so all command on the guild
+    @Profile("dev")
+    @Component
+    public static class DevCommandRegister implements CommandRegister{
+        
+        final private List<CommandsProvider.SlashCommand> CommandsProvider;
+
+        public DevCommandRegister(
+            List<CommandsProvider.SlashCommand> commandProviders
+        ){
+            this.CommandsProvider = commandProviders;
+        }
+
+        @Override
+        public void register(CommandContext ctx){
+            List<CommandData> commands = new ArrayList<>();
+            CommandsProvider.forEach(cp -> commands.add(cp.command()));
+
+            if(ctx.guild == null){
+                System.err.println("guild not found");
+                return;
+            }
+
+            ctx.guild.updateCommands()
+                .addCommands(commands)
+                .queue(
+                    ok -> System.out.println("Registered " + commands.size() + " commands for dev guild " + ctx.guild.getName()),
+                    err -> System.err.println("Failed to register dev guild commands: " + err.getMessage())                
+                );
+        }
+    }
+
+    //prod profil, so admin on guild and other on public
+    @Profile("prod")
+    @Component
+    public static class ProdCommandRegister implements CommandRegister{
+        
+        final private List<CommandsProvider.PublicSlashCommand> PublicSlashCommand;
+        final private List<CommandsProvider.AdminSlashCommand> AdminSlashCommand;
+
+        public ProdCommandRegister(
+            List<CommandsProvider.PublicSlashCommand> PublicSlashCommand,
+            List<CommandsProvider.AdminSlashCommand> AdminSlashCommand
+        ){
+            this.PublicSlashCommand = PublicSlashCommand;
+            this.AdminSlashCommand = AdminSlashCommand;
+        }
+
+        @Override
+        public void register(CommandContext ctx){
+            List<CommandData> PublicCommands = new ArrayList<>();
+            List<CommandData> AdminCommands = new ArrayList<>();
+
+            PublicSlashCommand.forEach(cp -> PublicCommands.add(cp.command()));
+            AdminSlashCommand.forEach(cp -> AdminCommands.add(cp.command()));
+        
+            if(ctx.guild == null){
+                System.err.println("guild not found");
+                return;
+            }
+
+            ctx.guild.updateCommands()
+                .addCommands(AdminCommands)
+                .queue(
+                    ok -> System.out.println("Registered " + AdminCommands.size() + " commands for admin guild " + ctx.guild.getName()),
+                    err -> System.err.println("Failed to register admin guild commands: " + err.getMessage())                
+                );
+
+            ctx.jda.updateCommands()
+                    .addCommands(PublicCommands)
+                    .queue(
+                            ok -> System.out.println("Registered " + PublicCommands.size() + " global commands"),
+                            err -> System.err.println("Failed to register global commands: " + err.getMessage())
+                    );
+        }
     }
 }
+
